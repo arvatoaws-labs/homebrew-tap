4989d785bdb4a8de40eee550beedb3fc1e0c6bed
Sebastian Tietz <sebastian.tietz@gmail.com>
2021-02-22 20:39:05 +0100

    assume role install

--- Formula/assume-role.rb
+++ Formula/assume-role.rb
@@ -1,23 +1,53 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class AssumeRole < Formula
-  desc ""
-  homepage ""
-  license ""
+  desc 'Arvato "assume-role" adoption including OKTA integration'
+  homepage "https://github.com/arvatoaws-labs/assume-role" # FIXME: add url to release
+  # sha256 "" //TODO: add hash when adding url to release
+  license "Apache License 2.0"
   head "https://github.com/arvatoaws-labs/assume-role.git"
 
-  # depends_on "cmake" => :build
+  bottle :unneeded
+
+  depends_on "awscli"
+  depends_on "gimme-aws-creds"
+  depends_on "jq"
+  depends_on "zsh" => :optional
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
+    libexec.install "assume-role"
+    bin.install_symlink libexec/"assume-role"
+    
+    # install autocompletion
+    zsh_completion.install "_assume-role"
+    
+    # TODO create a bash completer file
+    # bash_completion.install "aws_assume_role_completer"
+    # (bash_completion/"aws_bash_completer").write <<~EOS
+    #   fpath=(~/zsh_functions $fpath)
+    #   autoload -U compinit
+    #   compinit
+    # EOS
   end
+
+  def caveats
+    <<~EOS
+      Add the following line to your ~/.bashrc or ~/.zshrc:
+        source $(which assume-role)
+
+      To add profile autocomplete for Bash add:
+      
+        fpath=(~/zsh_functions $fpath)
+        autoload -U compinit
+        compinit
+    EOS
+  end
+
+  # def bash_completion
+  #   <<~EOS
+  #     fpath=(~/zsh_functions $fpath)
+  #     autoload -U compinit
+  #     compinit
+  #   EOS
+  # end
 
   test do
     # `test do` will create, run in and delete a temporary directory.
