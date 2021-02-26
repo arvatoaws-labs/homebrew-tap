2cc882c34fdeebc9ac993895190ad6890f0014d7
Sebastian Tietz <sebastian.tietz@gmail.com>
2021-02-26 15:07:16 +0100

    basic brew forms

--- Formula/assume-role.rb
+++ Formula/assume-role.rb
@@ -50,17 +50,6 @@ class AssumeRole < Formula
   # end
 
   test do
-    # TODO write test!
-    # assert_match "Assume", shell_output("#{bin}/assume-role")
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test assume-role`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "true"
+    assert_match "help", shell_output("#{bin}/assume-role -h")
   end
 end
--- Formula/sfn-ng.rb
+++ Formula/sfn-ng.rb
@@ -1,35 +1,15 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class SfnNg < Formula
-  desc ""
-  homepage ""
+  desc "Arvato SFN-NG"
+  homepage "https://github.com/arvatoaws-labs/sfn-ng"
   url "https://github.com/arvatoaws-labs/sfn-ng/archive/v0.2.17.tar.gz"
   sha256 "80ef53862bb3953e4ca647c2cff39ae7f0b2f9ed7e88397cea3d67cb9022df7d"
   license "GPL-3.0"
 
-  # depends_on "cmake" => :build
-
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
+    bin.install "sfn-ng"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test sfn-ng`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    assert_match "sfn-ng", shell_output("#{bin}/sfn-ng --version")
   end
 end
