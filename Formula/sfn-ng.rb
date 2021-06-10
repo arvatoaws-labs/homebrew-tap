800a2d390e7ccd08d4a59690c2bca9406f1b29ac
Sebastian Tietz <51152539+therealsebo@users.noreply.github.com>
2021-06-10 11:18:05 +0200

    Update sfn-ng to 2.18

--- Formula/sfn-ng.rb
+++ Formula/sfn-ng.rb
@@ -1,8 +1,8 @@
 class SfnNg < Formula
   desc "Arvato SFN-NG"
   homepage "https://github.com/arvatoaws-labs/sfn-ng"
-  url "https://github.com/arvatoaws-labs/sfn-ng/archive/v0.2.17.tar.gz"
-  sha256 "80ef53862bb3953e4ca647c2cff39ae7f0b2f9ed7e88397cea3d67cb9022df7d"
+  url "https://github.com/arvatoaws-labs/sfn-ng/archive/v0.2.18.tar.gz"
+  sha256 "8cc9934ae669096668ef7aee276a951f6c2f6b77a91053d1c6ad53fc842f81ca"
   license "GPL-3.0"
 
   # livecheck do
