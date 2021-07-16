7bd274e2cfc98943387265847d3827b73a8f383d
Patrick Robinson <patrick.robinson@bertelsmann.de>
2021-07-16 11:54:20 +0200

    Linting
    Signed-off-by: Patrick Robinson <patrick.robinson@bertelsmann.de>

--- Formula/assume-role.rb
+++ Formula/assume-role.rb
@@ -2,7 +2,7 @@ class AssumeRole < Formula
   desc 'Arvato "assume-role" adoption including OKTA integration'
   homepage "https://github.com/arvatoaws-labs/assume-role" # FIXME: add url to release
   # sha256 "" //TODO: add hash when adding url to release
-  license "Apache License 2.0"
+  license "Apache-2.0"
   head "https://github.com/arvatoaws-labs/assume-role.git"
 
   bottle :unneeded
@@ -15,11 +15,11 @@ class AssumeRole < Formula
   def install
     libexec.install "assume-role"
     bin.install_symlink libexec/"assume-role"
-    
+
     # install autocompletion
     zsh_completion.install "_assume-role"
-    
-    # TODO create a bash completer file
+
+    # TODO: create a bash completer file
     # bash_completion.install "aws_assume_role_completer"
     # (bash_completion/"aws_bash_completer").write <<~EOS
     #   fpath=(~/zsh_functions $fpath)
@@ -34,7 +34,7 @@ class AssumeRole < Formula
         source $(which assume-role)
 
       To add profile autocomplete for Bash add:
-      
+
         fpath=(~/zsh_functions $fpath)
         autoload -U compinit
         compinit
