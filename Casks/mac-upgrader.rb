bbe118c53e018d24e6a7c0976558b3af81690fc7
Patrick Robinson <patrick.robinson@bertelsmann.de>
2026-06-30 15:35:32 +0200

    feat: release mac upgrader
    Signed-off-by: Patrick Robinson <patrick.robinson@bertelsmann.de>

---
+++ Casks/mac-upgrader.rb
@@ -0,0 +1,19 @@
+cask "mac-upgrader" do
+  version "1.0.0"
+  sha256 "a96b576350cb19d033a4ab63c5a5284e2869f625a079915806b41e1a68c1630e"
+  url "https://github.com/arvatoaws/cloud-staff-tools/releases/download/mac-upgrader-v#{version}/MacUpgrader-#{version}.zip",
+      verified: "github.com/arvatoaws/cloud-staff-tools/"
+  name "Mac Upgrader"
+  desc "Runs all your system and package-manager upgrades in sequence"
+  homepage "https://github.com/arvatoaws/cloud-staff-tools/tree/main/mac-upgrader"
+
+  depends_on macos: :sonoma
+
+  app "MacUpgrader.app"
+
+  zap trash: [
+    "~/Library/Caches/com.cloudstaff.MacUpgrader",
+    "~/Library/Preferences/com.cloudstaff.MacUpgrader.plist",
+    "~/Library/Saved Application State/com.cloudstaff.MacUpgrader.savedState",
+  ]
+end
--- README.md
+++ README.md
@@ -5,5 +5,11 @@
 
 Or `brew tap arvatoaws-labs/tap` and then `brew install <formula>`.
 
+## How do I install these casks?
+`brew install --cask arvatoaws-labs/tap/<cask>`
+
+For example, the Mac Upgrader app:
+`brew install --cask arvatoaws-labs/tap/mac-upgrader`
+
 ## Documentation
 `brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
